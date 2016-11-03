262ddae4a5784f3913b174e233528a07ccb82172
Siyasanga <siyasanga@live.co.za>
2016-11-03 13:48:47 +0200

    Updated countRegNumber and added isTuesday

---
+++ isTuesday.js
@@ -0,0 +1,10 @@
+/*
+* This function
+*/
+function isTuesday(){
+  var today = new Date();
+  return (today.getDay() == 2);
+}
+const assert = require ('assert');
+assert.equal(isTuesday(), false)
+console.log(isTuesday());
