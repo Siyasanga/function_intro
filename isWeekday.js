72ffd3247844a5585d840c2c7813572931b02f01
Siyasanga <siyasanga@live.co.za>
2016-11-03 17:43:40 +0200

    Did some work on isWeekday.js

---
+++ isWeekday.js
@@ -0,0 +1,13 @@
+const date = new Date('2010-04-01');
+
+//return index between 0 and 6
+//console.log(date.getDay());
+
+//look up the week day text string from the Array
+//console.log(weekDays[date.getDay()]);
+
+function isWeekDay(day){
+  var weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  return (weekDays.indexOf(day) <= 4);
+}
+console.log(isWeekDay("Tuesday"));
